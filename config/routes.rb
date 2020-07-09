85c4f308989e7a568733ca1a2d5a257ac27d1747
Mitsuhiro.Akeda <mitsuhiro.akeda@MitsuhiroAkedanoMacBook-Air.local>
2020-07-09 14:28:46 +0900

    0709

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'books#index'
+  resources :books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
